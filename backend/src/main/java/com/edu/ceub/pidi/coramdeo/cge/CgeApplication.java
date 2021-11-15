9fbf79f04cd9d426ee412d3bdb0c9d0947c82e3a
Jose marcos <72453138+SakakibaraKun@users.noreply.github.com>
2021-11-15 16:27:33 -0300

    Cge project Created

---
+++ backend/src/main/java/com/edu/ceub/pidi/coramdeo/cge/CgeApplication.java
@@ -0,0 +1,13 @@
+package com.edu.ceub.pidi.coramdeo.cge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/edu/ceub/pidi/coramdeo/cge/CgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.ceub.pidi.coramdeo.cge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
