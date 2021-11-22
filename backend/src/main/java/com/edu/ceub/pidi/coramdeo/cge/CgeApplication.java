df7975266b6a6a3d795b31a9094f7d297d1f779d
Jose marcos <72453138+SakakibaraKun@users.noreply.github.com>
2021-11-22 17:37:15 -0300

    index redirect

--- backend/src/main/java/com/edu/ceub/pidi/coramdeo/cge/CgeApplication.java
+++ backend/src/main/java/com/edu/ceub/pidi/coramdeo/cge/CgeApplication.java
@@ -2,7 +2,9 @@ package com.edu.ceub.pidi.coramdeo.cge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = {"com.edu.ceub.pidi.coramdeo.cge.models"})
 @SpringBootApplication
 public class CgeApplication {
 
